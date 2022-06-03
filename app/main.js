d2999f1d7753dff42cc87e6a3b3cee51bb5bec4c
Arseniy Oguzov <oguzov.a@n-t.io>
2022-06-03 20:43:10 +0400

    Implement PING command

--- app/main.js
+++ app/main.js
@@ -3,8 +3,24 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+class RESP {
+    static fromStr(str) {
+        return `+${str.trim()}\r\n`
+    }
+}
+
 const server = net.createServer(socket => {
-  socket.pipe(socket);
+    socket.on('data', stream => {
+        const request = stream.toString('ascii').trim();
+        console.log({request});
+        switch(request) {
+            case 'PING':
+                socket.write(Buffer.from(RESP.fromStr('PONG')));
+                break;
+            default:
+                socket.write(Buffer.from('Unknown command'));
+        }
+    })
 });
 
 server.listen(6379, '127.0.0.1');
